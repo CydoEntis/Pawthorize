1920873f01f532524639492ccb61f7b4dc9bb20a
Cydo Entis <cydoentis@gmail.com>
2025-12-17 12:14:35 -0500

    feat: added IUserFactory.cs to allow users to define how to create their UserTypes from registration

---
+++ src/Pawthorize.Core/Abstractions/IUserFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace Pawthorize.Core.Abstractions;
+
+/// <summary>
+/// Factory for creating user entities during registration.
+/// Consumer implements this to define how to create their User type from registration request.
+/// </summary>
+/// <typeparam name="TUser">User type implementing IAuthenticatedUser</typeparam>
+/// <typeparam name="TRegisterRequest">Registration request type</typeparam>
+public interface IUserFactory<TUser, in TRegisterRequest>
+    where TUser : IAuthenticatedUser
+{
+    /// <summary>
+    /// Create a new user entity from registration request.
+    /// </summary>
+    /// <param name="request">Registration request (may contain custom fields if extended)</param>
+    /// <param name="passwordHash">Already-hashed password</param>
+    /// <returns>New user entity ready to be saved</returns>
+    TUser CreateUser(TRegisterRequest request, string passwordHash);
+}
